54c98b237dfb45219d0b227c91a228e360b2f084
NaimIslamLinux <naimislam2889.linux@gmail.com>
2026-07-23 23:26:51 +0600

    Save data and genarate HTML for delivery option container

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,19 @@
 export let cart = JSON.parse(localStorage.getItem('cart')) || [
 { 
   productId: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c", 
-  quantity: 1 
+  quantity: 1,
+  deliveryOptionId: "1"
 },
 { 
   productId: "1c079479-8586-494f-ab53-219325432536", 
-  quantity: 1 
+  quantity: 1,
+  deliveryOptionId: "2"
 
 }, 
 { 
   productId: "ee1f7c56-f977-40a4-9642-12ba5072e2b0", 
-  quantity: 1 
+  quantity: 1,
+  deliveryOptionId: "3"
 }
 ];
 
@@ -26,7 +29,8 @@ export function addToCart(productId) {
     } else {
     cart.push({
       productId: productId,
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId: "1"
     })
   }
   saveToStorage();
